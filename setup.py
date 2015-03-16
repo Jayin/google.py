b79da2ff21b1f6c8c62fe9ed61cbf0509277ab4c
Jayin Ton <tonjayin@gmail.com>
2015-03-16 11:46:26 +0800

    format

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup(
     packages=find_packages(),
     include_package_data = True,
     package_data={
-    'google': ['data/ips.json'],
-},
+        'google': ['data/ips.json'],
+    },
     entry_points={
         'console_scripts': [
             'google = google.core:main',
